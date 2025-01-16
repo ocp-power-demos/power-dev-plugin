53713fb7b87d90e0c81e25610411e0c1aeef3c55
Paul Bastide <pbastide@us.ibm.com>
2025-01-16 12:56:45 -0500

    fix: logging is not outputting, simplifying the code
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>

--- cmd/scanner/main.go
+++ cmd/scanner/main.go
@@ -1,15 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
-	"github.com/ocp-power-demos/power-dev-plugin/pkg/plugin"
+	"github.com/jaypipes/ghw"
 	"k8s.io/klog"
 )
 
 // Launch the scanner
 func main() {
-	devices, err := plugin.ScanRootForDevices()
+	devices, err := ScanRootForDevices()
 	if err != nil {
 		klog.Errorf("Could not scan devices, aborting %s", err)
 		os.Exit(2)
@@ -17,4 +18,26 @@ func main() {
 	for idx, device := range devices {
 		klog.Infof("%d - %s", idx, device)
 	}
+}
+
+// scans the local disk using ghw to find the blockdevices
+func ScanRootForDevices() ([]string, error) {
+	// relies on GHW_CHROOT=/host/dev
+	// lsblk -f --json --paths -s | jq -r '.blockdevices[] | select(.fstype != "xfs")' | grep mpath | grep -v fstype | sort -u | wc -l
+	// This may be the best way to get the devices.
+	block, err := ghw.Block()
+	if err != nil {
+		klog.Errorf("Error getting block storage info: %v", err)
+		return nil, err
+	}
+	devices := []string{}
+	fmt.Printf("DEVICE: %v\n", block)
+	for _, disk := range block.Disks {
+		fmt.Printf("    - DISK: %v\n", disk)
+		for _, part := range disk.Partitions {
+			fmt.Printf("        - PART: %v\n", part)
+			devices = append(devices, part.Name)
+		}
+	}
+	return devices, nil
 }
